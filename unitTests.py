735877e020a11b8bfaa45c631b1bd5993dcccd73
GleasonCarter <forgotmycodes1@gmail.com>
2017-11-30 15:33:41 -0500

    some more n-gram tests

--- unitTests.py
+++ unitTests.py
@@ -1,32 +1,40 @@
-import tkinterSearch as tS
+from tkinterSearch import ngram
 
 if __name__ == '__main__':
     print("Unit tests for ZZZ-earch")
     STOP_WORDS = ["the", "or", "and"]
     print("Test ngram()")
-
+    ## Long query
     ngramTest1 = "somebody once told me the world was gonna roll me"
-    correctResult1 = ["somebody once", "once told", "told me", "world was", "was gonna", "gonna roll", "roll me"]
+    correctBigram1 = ["somebody once", "once told", "told me", "world was", "was gonna", "gonna roll", "roll me"]
+    correctTrigram1 = ["somebody once told", "once told me", "world was gonna", "was gonna roll", "gonna roll me"]
 
     ngramTest2 = "i ain't the sharpest"
-    correctResult2 = ["i ain't"]
+    correctBigram2 = ["i ain't"]
 
     ngramTest3 = "tool in the shed"
-    correctResult3 = ["tool in"]
+    correctBigram3 = ["tool in"]
 
     ngramTest4 = "hey now"
-    correctResult4 = ["hey now"]
+    correctBigram4 = ["hey now"]
 
     ngramTest5 = "your'e an all-star"
-    correctResult5 = ["your'e an", "an all-star"]
+    correctBigram5 = ["your'e an", "an all-star"]
+    correctTrigram5 = ["your'e an all-star"]
+    print("Bigram tests...")
+    assert(correctBigram1 == ngram(2, ngramTest1, STOP_WORDS))
+    assert(correctBigram2 == ngram(2, ngramTest2 , STOP_WORDS))
+    assert(correctBigram3 == ngram(2, ngramTest3,STOP_WORDS))
+    assert(correctBigram4 == ngram(2, ngramTest4, STOP_WORDS))
+    assert(correctBigram5 == ngram(2, ngramTest5, STOP_WORDS))
+    print("Trigram tests...")
+    assert(correctTrigram1 == ngram(3, ngramTest1, STOP_WORDS))
+    assert([] == ngram(3, ngramTest2 , STOP_WORDS))
+    assert([] == ngram(3, ngramTest3,STOP_WORDS))
+    assert([] == ngram(3, ngramTest4, STOP_WORDS))
+    assert(correctTrigram5 == ngram(3, ngramTest5, STOP_WORDS))
 
-    assert(correctResult1 == tS.ngram(2, ngramTest1, STOP_WORDS))
-    assert(correctResult2 == tS.ngram(2, ngramTest2 , STOP_WORDS))
-    assert(correctResult3 == tS.ngram(2, ngramTest3,STOP_WORDS))
-    assert(correctResult4 == tS.ngram(2, ngramTest4, STOP_WORDS))
-    assert(correctResult5 == tS.ngram(2, ngramTest5, STOP_WORDS))
-
-
+    print("All ngram() tests passed!")
     print("Test stripPunctuation()")
 
     print("Test extractKeywords()")
